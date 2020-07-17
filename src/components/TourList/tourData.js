0a5bd415ca71f263d571cbd938b12100c03875ee
abdullah <hbkamk@gmail.com>
2020-07-17 20:23:48 +0600

    deploy

---
+++ src/components/TourList/tourData.js
@@ -0,0 +1,43 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    //img: "./img/newyork.jpeg",
+    img:
+      "https://images.pexels.com/photos/3560020/pexels-photo-3560020.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    //  "https://player.vimeo.com/external/309448537.sd.mp4?s=48b017844b9c4f1758ac43b944e5d39ffd50bc2c&profile_id=164&oauth2_token_id=57447761",
+    name: "new york bridge tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+  {
+    id: 2,
+    city: "paris",
+    //img: "./img/paris.jpeg",
+    img:
+      "https://images.pexels.com/photos/2427797/pexels-photo-2427797.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    //"https://www.pexels.com/video/bird-s-eye-view-of-ocean-waves-1918465/",
+    name: "paris lights tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+  {
+    id: 3,
+    city: "london",
+    // img: "./img/london.jpeg",
+    img:
+      "https://images.pexels.com/photos/1983162/pexels-photo-1983162.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    //"https://player.vimeo.com/external/269977920.sd.mp4?s=74aa30219677d2e69e48d43b439f164eef86c84e&profile_id=164&oauth2_token_id=57447761",
+    name: "london royal palace tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+];
