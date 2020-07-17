23c93756a6283770f9ebe78f7cbcb21387e39f6d
abdullah <hbkamk@gmail.com>
2020-07-17 20:24:16 +0600

    Updates

---
+++ precache-manifest.5b50a15fa7c1b476eaf4620df2811b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059f4b552db087573c42a343baa48f9b",
+    "url": "/city-tours/index.html"
+  },
+  {
+    "revision": "7e9656fab02024592f74",
+    "url": "/city-tours/static/css/main.b89da1b6.chunk.css"
+  },
+  {
+    "revision": "c039d575277354fc11a8",
+    "url": "/city-tours/static/js/2.1807f28e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/city-tours/static/js/2.1807f28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9656fab02024592f74",
+    "url": "/city-tours/static/js/main.8b436550.chunk.js"
+  },
+  {
+    "revision": "2adc47f9cb315dcb16be",
+    "url": "/city-tours/static/js/runtime-main.332042f5.js"
+  }
+]);
